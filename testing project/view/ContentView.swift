6515677cf414f00bc11f5daf6f09f4771d37b98b
TheAppMedia <iftikharhussain2230@gmail.com>
2022-08-25 15:22:16 +0500

    bg color applied

--- testing project/view/ContentView.swift
+++ testing project/view/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack{
             Text("HELLO WORLD FROM ISLAMABAD")
+                .background(Color.gray)
         }
         .padding(.horizontal)
         .foregroundColor(.white)
